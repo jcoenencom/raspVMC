100cebee5e562edd2da809d8dacd5462f3eb5a02
jcoenencom <jpcoenen@gmail.com>
2015-09-13 18:35:20 +0200

    adding CCEASE port new install script

--- install.bash
+++ install.bash
@@ -1,36 +1,59 @@
 #!/bin/bash
 
-#cleanup libraries
-rm /usr/lib/pymodules/python2.7/VMC.*
-cp VMC.pyc /usr/lib/pymodules/python2.7/
+#pythonpre requisite
 
-#server in /home/pi
-cp server.py ..
-chown pi ../server.py
-chmod a+x ../server.py
-
-#clients in /home/pi
-cp client?.py ../
-chown pi ../client?.py
-chmod a+x ../client?.py
+#sudo apt-get install python-serial
 
 
-#html pages in /var/www
-cp VMC?.html /var/www
-chown pi /var/www/VMC?.html
+echo first of all update all packages on the pi
 
-#cgi's in  apache default dir
-cp *.cgi /usr/lib/cgi-bin
-chown pi /usr/lib/cgi-bin/VMC*.cgi
-chmod a+x /usr/lib/cgi-bin/VMC*.cgi
+#sudo apt-get update
+#sudo apt-get upgrade
 
-#include libraries for json display in web pages
-cp -r json* /var/www
-chown -R pi /var/www/json*
-
-#configuration file
-if [ ! -d "/etc/VMC" ]; then 
-    mkdir "/etc/VMC"
+if ( ! dpkg -s python-serial 2> /dev/null | grep Status); then
+	echo python serial not installed, installing it
+	sudo apt-get install python-serial
 fi
 
-cp VMC.ini /etc/VMC/
+echo installing latest github version
+
+wget -q -O raspVMC.zip https://github.com/jcoenencom/raspVMC/archive/master.zip
+unzip raspVMC
+
+
+echo check if fhem is installed
+
+if ( dpkg -s fhem 2> /dev/null | grep Status); then
+	echo installed
+else
+    while [ "$OK" != "1" ]; do
+	echo FHEM is not installed do you want it to be installed "(Y/N)" ?
+	read -n 1 INST
+	if [ "$INST" = "Y" ]; then
+		echo " installing it"
+		echo Installation of FHEM, getting certificate
+                wget --no-check-certificate -qO - https://debian.fhem.de/archive.key | sudo apt-key add -
+		if (! grep -q fhem /etc/apt/sources.list ); then
+			echo add source in apt source file
+	                echo "deb https://debian.fhem.de/stable ./" | sudo tee -a /etc/apt/sources.list
+		fi
+		echo Check if apt-transport-https is installed
+		if ( ! dpkg -s apt-transport-https 2> /dev/null | grep Status ); then
+			echo add https transport
+        	        sudo apt-get install apt-transport-https
+		fi
+		echo installing fhem package
+		sudo apt-get install fhem
+		OK="1"
+	elif [ "$INST" != "N" ]; then
+		echo " reply Y or N please"
+	else
+		OK="1"
+	fi
+    done
+fi
+
+echo cleanup
+rm raspVMC.zip
+
+exit
