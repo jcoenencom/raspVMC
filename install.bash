be1ffc2e12e72518cb2242191f630d02d4c55779
JP Coenen <jpcoenen@gmail.com>
2015-09-16 10:22:22 +0200

    install.bash sduo needed for appanding to the fhem.cfg file

--- install.bash
+++ install.bash
@@ -71,7 +71,7 @@ sudo cp VMC.ini.new /etc/VMC/VMC.ini
 if [ -e /opt/fhem/fhem.cfg ]; then
 #file exist check if VMC already defined (normally should be adapted with device as stated in config run)
 	if [! grep VMC /opt/fhem/fhem.cfg ]; then
-		cat fhem.cfg >> /opt/fhem/fhem.cfg
+		sudo cat fhem.cfg >> /opt/fhem/fhem.cfg
 	fi
 fi
 echo patching inittab automatic restart in case of crash
